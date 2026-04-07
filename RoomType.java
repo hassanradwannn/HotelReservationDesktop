f6143af74b64f6252544309ecf7121b486334de7
yhyasales <yamr76941@gmail.com>
2026-04-07 22:11:45 +0200

    Add files via upload

---
+++ RoomType.java
@@ -0,0 +1,56 @@
+public class RoomType {
+    private int typeId;
+    private String typeName;
+    private double pricePerNight;
+    private int capacity;
+
+    public RoomType() {
+    }
+
+    public RoomType(int typeId, String typeName, double pricePerNight, int capacity) {
+        this.typeId = typeId;
+        this.typeName = typeName;
+        this.pricePerNight = pricePerNight;
+        this.capacity = capacity;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "RoomType ID: " + typeId +
+               ", Name: " + typeName +
+               ", Price/Night: " + pricePerNight +
+               ", Capacity: " + capacity;
+    }
+}
