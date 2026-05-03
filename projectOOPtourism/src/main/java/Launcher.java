b4f8b2a9795602521a7fd04c6ff4c64b39866174
Hassan Radwan <128387215+hassanradwannn@users.noreply.github.com>
2026-05-03 19:31:26 +0300

    launcher that launches instances to test multithreading with more efficiency

--- projectOOPtourism/src/main/java/Launcher.java
+++ projectOOPtourism/src/main/java/Launcher.java
@@ -1,5 +1,67 @@
+import java.io.File;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Launcher {
-    public static void main(String[] args) {
-        Main.main(args);
+    private static final int INSTANCE_COUNT = 3;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        List<Process> instances = new ArrayList<>();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            for (Process instance : instances) {
+                instance.destroy();
+            }
+        }));
+
+        for (int i = 1; i <= INSTANCE_COUNT; i++) {
+            List<String> command = buildLaunchCommand(args);
+            Process process = new ProcessBuilder(command)
+                    .directory(new File(System.getProperty("user.dir")))
+                    .inheritIO()
+                    .start();
+
+            instances.add(process);
+            System.out.println("Started hotel reservation instance " + i + " of " + INSTANCE_COUNT);
+        }
+
+        for (Process instance : instances) {
+            instance.waitFor();
+        }
+    }
+
+    private static List<String> buildLaunchCommand(String[] appArgs) {
+        List<String> command = new ArrayList<>();
+        command.add(javaExecutable());
+        command.addAll(childVmArguments());
+        command.add("-cp");
+        command.add(System.getProperty("java.class.path"));
+        command.add("Main");
+
+        for (String arg : appArgs) {
+            command.add(arg);
+        }
+
+        return command;
+    }
+
+    private static List<String> childVmArguments() {
+        List<String> vmArgs = new ArrayList<>();
+
+        for (String arg : ManagementFactory.getRuntimeMXBean().getInputArguments()) {
+            if (!arg.startsWith("-agentlib:")
+                    && !arg.startsWith("-javaagent:")
+                    && !arg.startsWith("-agentpath:")) {
+                vmArgs.add(arg);
+            }
+        }
+
+        return vmArgs;
+    }
+
+    private static String javaExecutable() {
+        return System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
     }
 }
